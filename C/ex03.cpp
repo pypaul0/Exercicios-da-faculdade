f8967e600f009ee58f74f516d2a2bdb215d411a0
Paulo <paulo.roberto28072002@gmail.com>
2021-08-22 13:19:36 -0300

    Ex03

---
+++ C/ex03.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char frase[30];
+	char frase_copy[30];
+	int i;
+	
+	printf("Digite uma frase: ");
+	gets(frase);
+	
+	for(i = 0; frase[i] != '\0'; i++) {
+		frase_copy[i] = frase[i];
+	}
+	
+	int t = strlen(frase)-1;
+
+	for (i = 0, t; frase[i] != '\0'; i++, t--) {
+		frase[i] = frase_copy[t];
+	}
+	printf("Frase codificada: %s", frase);
+}
